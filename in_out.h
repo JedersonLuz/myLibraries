1c480fd28f0695c9fe8a1fc50b21009d2d6d9e5e
JedersonLuz <jedersonalpha@gmail.com>
2018-04-10 20:59:31 -0300

    The first library to be inserted in the repository, which has the name in_out.h is being implemented with the purpose of facilitating the entry and exit of data in the C language.

---
+++ in_out.h
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <stdbool.h>
+
+char * input (const char * format, ...) {
+  char *value = malloc(255);
+  va_list arg;
+
+  va_start (arg, format);
+  vfprintf (stdout, format, arg);
+  va_end (arg);
+
+  setbuf(stdin, NULL);
+  scanf("%[^\n]s", value);
+
+  return value;
+}
+
+int str4int (char *message) {
+  if (!strcmp(message, "0")) {
+    return 0;
+  } else {
+    int value = atoi(message);
+    while (value == 0) {
+      message = input("Enter a valid value!\n");
+      value = atoi(message);
+      if (!strcmp(message, "0"))
+        break;
+    }
+    return value;
+  }
+}
+
+float str4float (char *message) {
+  if (!strcmp(message, "0")) {
+    return 0;
+  } else {
+    float value = atof(message);
+    while (value == 0) {
+      message = input("Enter a valid value!\n");
+      value = atof(message);
+      if (!strcmp(message, "0"))
+        break;
+    }
+    return value;
+  }
+}
+
+void sysPause (const char * format, ...) {
+  va_list arg;
+
+  va_start (arg, format);
+  vfprintf (stdout, format, arg);
+  va_end (arg);
+
+  setbuf(stdin, NULL);
+  getchar();
+}
---
+++ test.c
@@ -0,0 +1,9 @@
+#include "in_out.h"
+
+int main () {
+    int value = str4int(input("Informe %d valor: ", 1));
+
+    printf("%d\n", value);
+
+    return 0;
+}
